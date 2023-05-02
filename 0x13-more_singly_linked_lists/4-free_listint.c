e5fa8425b976e345858462a9d1039cfc0706f491
Manar <manarkhaledx@outlook.com>
2023-05-02 03:05:48 -0700

    x

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,3 +1,18 @@
-4-free_listint.c
+#include "lists.h"
 
+/**
+ * free_listint - frees a linked list
+ * @head: listint_t list to be freed
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
 
