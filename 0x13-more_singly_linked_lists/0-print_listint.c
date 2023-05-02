014f0b1d70c55b3ebcd1b161298a12027426e9a5
Manar <manarkhaledx@outlook.com>
2023-05-02 03:03:42 -0700

    x

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,28 +8,15 @@
  */
 size_t print_listint(const listint_t *h)
 {
-<<<<<<< HEAD
-	size_t nu = 0;
+	size_t num = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		nu++;
+		num++;
 		h = h->next;
 	}
 
-	return (nu);
-=======
- size_t num = 0;
-
- while (h)
- {
- printf("%d\n", h->n);
- num++;
- h = h->next;
- }
-
- return (num);
->>>>>>> 2ac2154981d9476da4d595455990ea02cb4d7cd6
+	return (num);
 }
 
